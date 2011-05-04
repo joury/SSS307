ac305d0aa46875db33133e27ab7bb47481889809
Jasper Tulp <jasper_tulp@hotmail.com>
2011-05-05 01:15:27 +0200

    Nederlands.php toegevoegd aan Languages

---
+++ languages/Nederlands.php
@@ -0,0 +1,40 @@
+<?php
+$Language = array(
+    // General
+    "NoDB" => "Geen Database connectie beschikbaar",
+    "ErrorOccured" => "De volgende fout(en) is zich opgetreden",
+    "FieldEmpty" => "veld is leeg",
+    "UnknownError" => "Een onbekende fout is opgetreden.",
+    "UserLost" => "De gebruiker informatie is niet opvraagbaar, probeer opnieuw in te loggen (Cookie verstreken).",
+
+    // Simple Words
+    "Login" => "Login",
+    "Register" => "Registreren",
+    "Password" => "Wachtwoord",
+    "Username" => "Gebruikersnaam",
+    "ConfirmPassword" => "Bevestig wachtwoord",
+
+    // File handling
+    "FileBig" => "Bestand is te groot, bestand is",
+    "FileSize" => "maar mag zijn",
+    "FileType" => "ongeldig bestandstype",
+    "SaveError" => "Bestand kan niet worden opgeslagen als gevolg van een onbekende reden",
+
+    // Buttons/Forms
+    "ChangePass" => "Verander wachtwoord.",
+    "ProfileInfo" => "Profiel informatie",
+
+    // Login/Register
+    "AccountwName" => "Account met de naam",
+    "AccountExist" => "bestaat al.",
+    "AccountEmailUsername" => "Ofwel de gebruikersnaam of het e-mailadres is al in gebruik.",
+    "LoginFailed" => "Ofwel de gebruikersnaam bestaat niet of het wachtwoord is onjuist.",
+    "EmailInvalid" => "Het ingevulde e-mailadres is geen geldig e-mailadres.",
+    "EmailEmpty" => "Het e-mailadres veld moet worden ingevuld.",
+    "PasswordChanged" => "Wachtwoord is succesvol gewijzigd.",
+    "PasswordRules" => "Het wachtwoord voldoet niet aan de regels (beweeg de cursor over het plaatje naast het veld voor meer informatie)",
+    "PasswordMatch" => "Het wachtwoord en het bevestigingsveld zijn niet hetzelfde.",
+    "PasswordEmpty" => "Beide wachtwoord velden moeten ingevuld worden volgens de regels.",
+    "PassChangeMatch" => "Het oude wachtwoord komt niet overeen met het huidige wachtwoord.",
+);
+?>
